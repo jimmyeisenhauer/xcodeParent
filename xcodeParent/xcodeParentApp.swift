61e650e2949d1d6bd8e54d8a5ded1c445b6b1f0f
Jimmy Eisenhauer <james.eisenhauer@gmail.com>
2022-10-03 22:13:38 -0700

    Initial Commit

---
+++ xcodeParent/xcodeParentApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodeParentApp.swift
+//  xcodeParent
+//
+//  Created by Jimmy Eisenhauer on 10/3/22.
+//
+
+import SwiftUI
+
+@main
+struct xcodeParentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
